7e075d4af7f74da77469cf879422fdb6fb4ce3ae
qadyuanzai <42380987+qadyuanzai@users.noreply.github.com>
2019-06-13 23:37:57 +0800

    2019/06/13 23:37

--- PlaneGame0.4/src/cn/sxt/game/MyGameFrame.java
+++ PlaneGame0.4/src/cn/sxt/game/MyGameFrame.java
@@ -3,8 +3,11 @@ package cn.sxt.game;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 
 /**
@@ -16,15 +19,15 @@ public class MyGameFrame extends JFrame {
 	
 	Image plane = GameUtil.getImage("images/plane.png")	;
 	Image bg = GameUtil.getImage("images/bg.jpg");
-	int planeX = 250;
-	int planeY = 250;
+	
+	Plane p = new Plane(plane, 250, 250);
+	
 	
 	//自动调用，不能手动，g相当画笔
 	@Override
 	public void paint(Graphics g) {
 		g.drawImage(bg, 0, 0, null);
-		g.drawImage(plane, planeX, planeY, null);
-		planeX++;
+		p.drawSelf(g);
 	}
 	
 	//帮助我们反复重画窗口
@@ -32,7 +35,6 @@ public class MyGameFrame extends JFrame {
 		public void run() {
 			while(true) {
 				repaint();//重画
-				
 				try {
 					Thread.sleep(40);
 				} catch (InterruptedException e) {
@@ -42,7 +44,24 @@ public class MyGameFrame extends JFrame {
 		}
 	}
 	
+	//定义键盘监听的内部类
+	class KeyMonitor extends KeyAdapter{
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			p.addDirection(e);
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			p.minusDirection(e);
+		}
+		
+	}
 	
+	
+	//
+	//
 	//初始化窗口
 	public void launchFrame() {
 		this.setTitle("Percy的作品");
@@ -57,7 +76,8 @@ public class MyGameFrame extends JFrame {
 			}
 		});
 		
-		new PaintThread().start();
+		new PaintThread().start();//启动重画窗口线程
+		addKeyListener(new KeyMonitor());//给窗口增加键盘监听
 	}
 	
 	public static void main(String[] args) {
@@ -65,5 +85,3 @@ public class MyGameFrame extends JFrame {
 		f.launchFrame();
 	}
 }
-
-
