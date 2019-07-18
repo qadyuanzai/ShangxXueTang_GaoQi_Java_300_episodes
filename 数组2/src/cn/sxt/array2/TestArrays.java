68a2199adaead4a5de7bef1b677d2d62e91aa548
qadyuanzai <42380987+qadyuanzai@users.noreply.github.com>
2019-07-18 21:36:08 +0800

    2019/07/18 21:36

---
+++ 数组2/src/cn/sxt/array2/TestArrays.java
@@ -0,0 +1,16 @@
+package cn.sxt.array2;
+
+import java.util.Arrays;
+/**
+ * 测试java.util.Arrays工具类的使用
+ * @author qadyuanzai
+ *
+ */
+
+public class TestArrays {
+	public static void main(String[] args) {
+		int[] a = {10, 20, 30};
+		
+		System.out.println(Arrays.toString(a));
+	}
+}
