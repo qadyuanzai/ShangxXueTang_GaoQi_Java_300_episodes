6afb75bc3baae0653cf42aa1334eb8210cab1609
qadyuanzai <42380987+qadyuanzai@users.noreply.github.com>
2019-07-19 18:57:04 +0800

    2019/07/19 18:57

--- 数组2/src/cn/sxt/array2/TestBinarySearch.java
+++ 数组2/src/cn/sxt/array2/TestBinarySearch.java
@@ -4,26 +4,29 @@ import java.util.Arrays;
 
 public class TestBinarySearch {
 	public static void main(String[] args) {
-		int[] arr = {30, 20, 50, 10, 80, 9, 7, 12, 100, 40, 8};
-		int value = 10;
-		
+		int[] arr = {30, 20, 50, 10, 80, 9, 7, 12, 100, 40, 8};		
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		System.out.println(myBinarySearch(arr, 11));
+	}
+
+	public static int myBinarySearch(int[] arr, int value) {
 		int low = 0;
 		int high = arr.length - 1;
-		Arrays.sort(arr);
-		
-		while(low<high) {
+		int index = -1;
+		while(low <= high) {
 			int mid = (low + high) / 2;
 			if(arr[mid] > value) {
-				high = mid;
+				high = mid - 1;
 			}else if(arr[mid] < value) {
-				low = mid;
+				low = mid + 1;
 			}else {
-				
+				index = mid;
+				break;
 			}
-			
 		}
-		
-		
-		
+		return index;
 	}
 }
+
+
