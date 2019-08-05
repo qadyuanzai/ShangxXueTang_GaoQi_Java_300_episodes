faae26a2235bff27e3455b2ad4ba7fcf64f61437
qadyuanzai <42380987+qadyuanzai@users.noreply.github.com>
2019-08-06 00:22:43 +0800

    2019/08/06 00:22

---
+++ 容器/src/cn/sxt/collection/TestMap.java
@@ -0,0 +1,22 @@
+package cn.sxt.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ²âÊÔHashMapµÄÊ¹ÓÃ
+ * @author qadyuanzai
+ *
+ */
+public class TestMap {
+	public static void main(String[] args) {
+		Map<Integer, String> m1 = new HashMap<>();
+		
+		m1.put(1, "one");
+		m1.put(2, "two");
+		m1.put(3, "three");
+
+		System.out.println(m1.get(1));
+		
+	}
+}
---
+++ 容器/src/cn/sxt/collection/TestVector.java
@@ -0,0 +1,11 @@
+package cn.sxt.collection;
+
+import java.util.List;
+import java.util.Vector;
+
+public class TestVector {
+	public static void main(String[] args) {
+		List<String> a = new Vector<String>();
+		
+ 	}
+}
